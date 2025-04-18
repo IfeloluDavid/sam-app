f60badea5ae1b4af590cbf2cd0d12eaf6682ac9e
Ifelolu David <ifelolud@gmail.com>
2025-04-18 17:37:40 +0100

    Add Canary deployment configuration to SAM - Test Canary

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello David Oladimeji",
+            "message": "hello David Oladimeji - I'm using Canary Deployments",
             # "location": ip.text.replace("\n", "")
         }),
     }
