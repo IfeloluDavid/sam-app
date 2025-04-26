fd257596cbcf40e1a97dded0af842706864265b2
Ifelolu David <ifelolud@gmail.com>
2025-04-26 13:59:36 +0100

    Canary Deployment 2

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Oladimeji David",
+            "message": "hello Oladimeji David - Canary Deployment",
             # "location": ip.text.replace("\n", "")
         }),
     }
